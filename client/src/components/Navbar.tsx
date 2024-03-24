15eaa8b1fb08052fef0f45c25292db3efa8efb28
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-03-24 20:11:28 +0530

    clientpush

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { HiMenuAlt1 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+const Navbar = () => {
+    return (
+        <nav className="w-full flex md:justify-content justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial items-center"></div>
+        </nav>
+    );
+};
+
+export default Navbar;
